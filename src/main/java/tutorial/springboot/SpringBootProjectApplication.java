21fa445854231522203987f2ad469858c24c6223
Parthsony <parthsony@live.in>
2019-02-11 18:04:41 +0530

    3 Refactored code

--- src/main/java/tutorial/springboot/SpringBootProjectApplication.java
+++ src/main/java/tutorial/springboot/SpringBootProjectApplication.java
@@ -1,11 +1,9 @@
-package com.example.demo;
+package tutorial.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("tutorial.springboot")
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
--- src/main/java/tutorial/springboot/service/SurveyService.java
+++ src/main/java/tutorial/springboot/service/SurveyService.java
@@ -21,7 +21,44 @@ public class SurveyService {
 
 	private static List<Survey> surveys = new ArrayList<>();
 
+	public List<Survey> retrieveAllSurveys() {
+		return surveys;
+	}
+
+	public Survey retrieveSurvey(String surveyId) {
+		return surveys.stream().filter(survey -> surveyId.equals(survey.getId())).findAny().orElse(null);
+	}
+
+	public List<Question> retrieveQuestions(String surveyId) {
+		Survey retrieveSurvey = retrieveSurvey(surveyId);
+		return null != retrieveSurvey ? retrieveSurvey.getQuestions() : new ArrayList<>();
+	}
+
+	public Question retrieveQuestion(String surveyId, String questionId) {
+
+		return retrieveSurvey(surveyId) == null ? null
+				: retrieveSurvey(surveyId).getQuestions().stream()
+						.filter(question -> questionId.equals(question.getId())).findAny().orElse(null);
+	}
+
+	private SecureRandom random = new SecureRandom();
+
+	public Question addQuestion(String surveyId, Question question) {
+
+		Survey survey = retrieveSurvey(surveyId);
+
+		if (survey == null) {
+			return null;
+		}
+
+		question.setId(new BigInteger(130, random).toString(32));
+		survey.getQuestions().add(question);
+
+		return question;
+	}
+
 	static {
+
 		Question question1 = new Question("Question1", "Largest Country in the World", RUSSIA,
 				Arrays.asList(INDIA, RUSSIA, UNITED_STATES, CHINA));
 		Question question2 = new Question("Question2", "Most Populus Country in the World", CHINA,
@@ -36,62 +73,6 @@ public class SurveyService {
 		Survey survey = new Survey("Survey1", "My Favorite Survey", "Description of the Survey", questions);
 
 		surveys.add(survey);
-	}
-
-	public List<Survey> retrieveAllSurveys() {
-		return surveys;
-	}
-
-	public Survey retrieveSurvey(String surveyId) {
-		for (Survey survey : surveys) {
-			if (survey.getId().equals(surveyId)) {
-				return survey;
-			}
-		}
-		return null;
-	}
-
-	public List<Question> retrieveQuestions(String surveyId) {
-		Survey survey = retrieveSurvey(surveyId);
-
-		if (survey == null) {
-			return new ArrayList<>();
-		}
-
-		return survey.getQuestions();
-	}
-
-	public Question retrieveQuestion(String surveyId, String questionId) {
-		Survey survey = retrieveSurvey(surveyId);
-
-		if (survey == null) {
-			return null;
-		}
-
-		for (Question question : survey.getQuestions()) {
-			if (question.getId().equals(questionId)) {
-				return question;
-			}
-		}
-
-		return null;
-	}
-
-	private SecureRandom random = new SecureRandom();
-
-	public Question addQuestion(String surveyId, Question question) {
-		Survey survey = retrieveSurvey(surveyId);
-
-		if (survey == null) {
-			return null;
-		}
-
-		String randomId = new BigInteger(130, random).toString(32);
-		question.setId(randomId);
-
-		survey.getQuestions().add(question);
-
-		return question;
 	}
 
 }
